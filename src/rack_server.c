00e875469d9256b2248f9de03124585d33df8490
Neeraj Krishnan <neeraj@neeraj-ubuntu12.(none)>
2013-09-25 19:27:18 -0400

    fixes

--- src/rack_server.c
+++ src/rack_server.c
@@ -1,12 +1,13 @@
 
-#include "event2/buffer.h"
-#include "event2/event.h"
-#include "event2/event_compat.h"
-#include "event2/http.h"
-#include "event2/http_compat.h"
-#include "event2/http_struct.h"
-#include "event2/rpc.h"
-#include "event2/tag.h"
+#include <event.h>
+#include <event2/buffer.h>
+#include <event2/event.h>
+#include <event2/event_compat.h>
+#include <event2/http.h>
+#include <event2/http_compat.h>
+#include <event2/http_struct.h>
+#include <event2/rpc.h>
+#include <event2/tag.h>
 
 #include <signal.h>
 
@@ -17,30 +18,29 @@ EVRPC_HEADER(RequestVote, request_vote_request, request_vote_response)
 EVRPC_GENERATE(RequestVote, request_vote_request, request_vote_response)
 
 static void request_vote_cb(EVRPC_STRUCT(RequestVote)* rpc, void * arg) {
+  /*
   struct request_vote_reply* rvr = rpc->reply;
   rvr->term = 1;
   rvr->vote_granted = 1;
   EVRPC_REQUEST_DONE(rpc);
+  */
 }
 
 int main( int argc, char * argv[] ) {
 
-  struct event_base * base;
-  struct evhttp * http;
+  struct event_base * base = event_base_new();
+  struct evhttp * http = evhttp_new(base);
 
   int port = 1979;
   char const * host = "127.0.0.1";
+  evhttp_bind_socket_with_handle(http, host, port);
 
   signal(SIGPIPE, SIG_IGN);
-
-  base = event_base_new();
-  http = evhttp_new(base);
-  evhttp_bind_socket_with_handle(http, host, port);
 
   struct evrpc_base * rpc_base;
   rpc_base = evrpc_init(http);
 
   EVRPC_REGISTER(rpc_base, RequestVote, request_vote_request, request_vote_response, request_vote_cb, NULL);
 
-  event_dispatch();
+  event_base_dispatch(base);
 }
