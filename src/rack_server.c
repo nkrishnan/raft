9e151512f1a5a36a0823deede8b2cc7e3526ab60
Neeraj Krishnan <neeraj@neeraj-ubuntu12.(none)>
2013-09-27 00:17:19 -0400

    latest

--- src/rack_server.c
+++ src/rack_server.c
@@ -9,13 +9,30 @@
 #include <event2/rpc.h>
 #include <event2/tag.h>
 
+#include <stdlib.h>
 #include <signal.h>
+#include <stdio.h>
 
 #include "rack.gen.h"
-#include "rack_server.h"
+#include "STATE.h"
 
 EVRPC_HEADER(RequestVote, request_vote_request, request_vote_response)
 EVRPC_GENERATE(RequestVote, request_vote_request, request_vote_response)
+
+void issue_request_vote(struct STATE * s) {
+  struct IDENTITY * i = get_peer_ids(s);
+  while(i) {
+    struct request_vote_request * rvr = request_vote_request_new();
+    EVTAG_ASSIGN(rvr, 
+  }
+}
+
+void election_cb(evutil_socket_t fd, short event, void * arg) {
+  struct STATE * s = (struct STATE *) arg;
+  increment_term(s);
+  set_role(s, Candidate);
+  issue_request_vote(s);
+}
 
 static void request_vote_cb(EVRPC_STRUCT(RequestVote)* rpc, void * arg) {
 
@@ -41,5 +58,6 @@ int main( int argc, char * argv[] ) {
 
   EVRPC_REGISTER(rpc_base, RequestVote, request_vote_request, request_vote_response, request_vote_cb, NULL);
 
+  struct STATE * s = init_state(base);
   event_base_dispatch(base);
 }
--- src/rack_server.h
+++ src/rack_server.h
@@ -1,12 +1,15 @@
 
+#include <event.h>
+
 enum { MAX_HOST_NAME_LEN = 256 };
+enum Role { Follower, Candidate, Leader };
 
 struct LEADER_STATE {
   uint64_t * next_index;
   uint64_t * match_index;
 };
 
-void reset_leader_state( struct LEADER_STATE * );
+void reset_leader_state(struct LEADER_STATE *);
 
 struct PERSISTENT_STATE {
   uint64_t current_term;
@@ -15,7 +18,12 @@ struct PERSISTENT_STATE {
 
 struct STATE {
   struct PERSISTENT_STATE persistent_state;
+  struct LEADER_STATE leader_state;
   uint64_t commit_index;
   uint64_t last_applied;
-  struct LEADER_STATE leader_state;
+  Role role;
+  struct event * election_event;
+  struct timeval election_timeout;
 };
+
+struct STATE * init_state();
